54fc479fcc0f0778d90cf0d4a9d9b31ce24f5923
Leonard Xu <xbjtdcq@gmail.com>
2021-08-08 17:47:21 +0800

    test master version

--- docs/conf.py
+++ docs/conf.py
@@ -32,9 +32,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -94,10 +94,6 @@ html_context = {
         '_static/theme_overrides.css',  # overrides for wide tables in RTD theme
     ],
 }
-
-# multiple version supports
-import sys, os
-sys.path.insert(0, os.path.abspath('.'))
 
 try:
     html_context
